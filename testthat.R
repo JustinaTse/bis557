28024a7fb50ee1d50289a1d9c154c5144cf94a18
Justina Xie <justinakiwi@gmail.com>
2020-09-10 19:08:31 -0400

    Delete a file

--- testthat.R
+++
@@ -1,7 +0,0 @@
-context('Testing grab coefficients.")
-
-library(palmerpenguin)
-data(penguin)
-fit<-lm(bill_length_mm ~., data = penguins[,-8])
-coefs<-as.vector(grab_coeffs(fit))
-expect_equal(round(grab_coeffs(fit)),c(15,10,6,-1,0,0,0,0,2))
